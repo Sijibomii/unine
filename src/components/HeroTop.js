2d996e730c1c65baea40b9aba04b00141a12e9b3
unknown <sijibomiolajubu@gmail.com>
2022-11-01 22:12:36 +0100

    sm hero done

--- src/components/HeroTop.js
+++ src/components/HeroTop.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroTop = () => {
   return (
-    <div className='px-6 mt-4 py-6'>
+    <div className='px-4 mt-4 py-6'>
       <div className=''>
         <h2 className='text-4xl text-white font-semibold text-center leading'>More Payment Options<br /> Better than Cash</h2>
         <p className='calmer-white text-white text-center mt-8 font-medium'>With UNINE you can access more than 240 million customers out there, as well as
