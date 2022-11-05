14d15198750d5987ba2edd6544310928c9ee3fb0
unknown <sijibomiolajubu@gmail.com>
2022-11-05 03:02:02 +0100

    changes

--- src/components/Info.js
+++ src/components/Info.js
@@ -11,7 +11,7 @@ const Info = () => {
         <p className='font-normal text-lg calmer-white text-center mt-8'>Want to pay anything so easy with the touch of a finger. Through UNINE you can make practically any transaction</p>
       </div>
       <div className='py-4 px-12 mt-6'>
-        <div className='flex flex-col justify-center bg-[#FFF] px-6 py-4 rounded-2xl'>
+        <div className='flex flex-col justify-center bg-[#FFF] px-6 py-8 rounded-2xl'>
           <div className='top'>
             <img src={Info1} className="h-16 w-22" />
           </div>
@@ -21,7 +21,7 @@ const Info = () => {
           </div>
         </div>
 
-        <div className='flex flex-col justify-center bg-[#72E484] mt-10 px-6 py-4 rounded-2xl'>
+        <div className='flex flex-col justify-center bg-[#72E484] mt-10 px-6 py-8 rounded-2xl'>
           <div className='top bg-[#FFF] w-1/4 flex items-center justify-center rounded-2xl mt-2'>
             <img src={Info2} className="h-16 w-22" />
           </div>
@@ -31,23 +31,23 @@ const Info = () => {
           </div>
         </div>
 
-        <div className='flex flex-col justify-center bg-[#FFF] mt-10 px-6 py-10 rounded-2xl'>
+        <div className='flex flex-col justify-center bg-[#FFF] mt-10 px-6 py-8 rounded-2xl'>
           <div className='top bg-[#FFF] w-1/4 flex items-center justify-center rounded-2xl'>
             <img src={Info3} className="h-16 w-22" />
           </div>
           <div className='bottom mt-14'>
-            <h4 className='b text-2xl font-bold'>No hidden fees</h4>
-            <p className='calmer-white mt-4 text-xl mt-8'>You will only be charged for selling goods or requesting payment</p>
+            <h4 className='b text-xl font-bold'>No hidden fees</h4>
+            <p className='calmer-white mt-4 text-md mt-8'>You will only be charged for selling goods or requesting payment</p>
           </div>
         </div>
 
-        <div className='flex flex-col justify-center bg-[#FFF] mt-10 px-6 py-10 rounded-2xl'>
+        <div className='flex flex-col justify-center bg-[#FFF] mt-10 px-6 py-8 rounded-2xl'>
           <div className='top bg-[#FFF] w-1/4 flex items-center justify-center rounded-2xl'>
             <img src={Info4} className="h-16 w-22" />
           </div>
           <div className='bottom mt-14'>
-            <h4 className='b text-2xl font-bold'>Free Shipping</h4>
-            <p className='calmer-white mt-4 text-xl mt-8'>Changed your mind after buying? Send back the item you purchased</p>
+            <h4 className='b text-xl font-bold'>Free Shipping</h4>
+            <p className='calmer-white mt-4 text-md mt-8'>Changed your mind after buying? Send back the item you purchased</p>
           </div>
         </div>
 
