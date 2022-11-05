557a4c2ad3b44166fe1aed7b25ca1db90ca5faac
unknown <sijibomiolajubu@gmail.com>
2022-11-05 02:44:55 +0100

    changes

--- src/components/HeroTop.js
+++ src/components/HeroTop.js
@@ -5,7 +5,7 @@ const HeroTop = () => {
     <div className='px-4 mt-4 py-6'>
       <div className=''>
         <h2 className='text-4xl text-white font-semibold text-center leading'>More Payment Options<br /> Better than Cash</h2>
-        <p className='calmer-white text-white text-center mt-8 font-medium'>With UNINE you can access more than 240 million customers out there, as well as
+        <p className='text-white text-center mt-8 font-light'>With UNINE you can access more than 240 million customers out there, as well as
         well as offering management tools options and payment methods.</p>
       </div>
       {/* lg screen play store here */}
